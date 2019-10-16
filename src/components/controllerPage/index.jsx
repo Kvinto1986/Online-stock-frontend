20083c3fc4f2a60986c0a032ffa3975ac5253cec
Yan.Palyn <Kvinto1986@gmail.com>
2019-10-16 17:23:13 +0300

    fix callback

--- src/components/controllerPage/index.jsx
+++ src/components/controllerPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import ControlTTNPage from './controlTTNpage'
 import {useGetTtn, useEditTtn} from '../../api/apiRequests'
 import {useReset} from '../../hooks/hook'
@@ -7,9 +7,12 @@ import successSwal from '../warehousesPage/successSwal'
 export default () => {
     const [keyPage, resetKey] = useReset()
 
-    const handleResetForm = () => {
-        successSwal(resetKey, 'TTN successfully save')
-    }
+    const handleResetForm = useCallback(
+        () => {
+            successSwal(resetKey, 'TTN successfully save')
+        },
+        [keyPage, resetKey],
+    )
 
     const [getTtn, ttns, getTtnError] = useGetTtn()
     const [editTtn] = useEditTtn(handleResetForm)
