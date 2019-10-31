dcaf3da5deb78b7f6840d4b0768dcb3251c074bc
Maksim Yepifanau <Maksim.Yepifanau@itechart-group.com>
2019-10-31 13:51:44 +0300

    success swal as separated file, add swal at new company create form

--- src/components/registerCompanyAdmin/registerCompanyAdmin.jsx
+++ src/components/registerCompanyAdmin/registerCompanyAdmin.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import CompanyAdminPage from './registerCompanyAdminPage'
 import {useStorelessApiCallback} from '../../hooks/hook'
 import {addCompanyAdmin} from '../../api/companyAdmins'
+import {useReset} from './../../hooks/hook'
+import succesAlert from '../swal/succesSwal'
 
 export default () => {
+    const [keyReset, reset] = useReset()
+    const [onSubmit, errors] = useStorelessApiCallback(
+        addCompanyAdmin, 
+        () => succesAlert('Success!','Company admin has been registred', 2000, false, reset)
+    )
 
-    const [onSubmit,errors] = useStorelessApiCallback(addCompanyAdmin, ()=>{})
     return <CompanyAdminPage
         onSubmit={onSubmit}
         errors={errors}
+        keyReset={keyReset}
     />
-};
+}
 
