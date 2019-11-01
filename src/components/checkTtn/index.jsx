a476bce1fc241f001d50a1145ef16ecd6bcb4814
Yan.Palyn <Kvinto1986@gmail.com>
2019-11-01 12:12:05 +0300

    add ttn check table, fix controller report

--- src/components/checkTtn/index.jsx
+++ src/components/checkTtn/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import {useGetTtn, useEditTtn} from '../../api/apiRequests'
+import React, {useCallback} from 'react'
+import {useGetTtn, useDelTtn} from '../../api/apiRequests'
 import CheckPage from './checkTtnPage'
+import {useReset} from '../../hooks/hook'
+import successSwal from '../warehousesPage/successSwal'
 
 export default () => {
+    const [keyPage, resetKey] = useReset()
 
-    const [getTtn,ttns, getTtnError] = useGetTtn()
-    const [editTtn, , editTtnError] = useEditTtn()
-    const [deleteTtn, , deleteTtnError] = useEditTtn()
+    const handleResetForm = useCallback(
+        () => {
+            successSwal(resetKey, 'TTN successfully deleted')
+        },
+        [resetKey],
+    )
+
+    const [getTtn, ttns, getTtnError] = useGetTtn()
+    const [deleteTtn] = useDelTtn(handleResetForm)
 
     return <CheckPage
+        key={keyPage}
         searchTtn={getTtn}
         ttns={ttns}
         getTtnError={getTtnError}
-        editTtn={editTtn}
-        editTtnError={editTtnError}
         deleteTtn={deleteTtn}
-        deleteTtnError={deleteTtnError}
     />
 }
