7926c95c24b471e45a0c8e253b55d93f85a3104c
Yan.Palyn <Kvinto1986@gmail.com>
2019-11-05 11:52:02 +0300

    some fixes

--- src/components/allCarrier/tableRows.jsx
+++ src/components/allCarrier/tableRows.jsx
@@ -8,58 +8,58 @@ import useStyles from './allCarrierStyle'
 import AddIcon from '@material-ui/icons/Add'
 
 const TableRowsComponent = ({row, tableCells, handleEdit, handleNewCarrier, removeItem}) => {
-  const classes = useStyles()
-  const {id, company, email, tel} = row
-  return (
-    <TableRow>
-      {
-        tableCells.map((item) => {
-          const {typeComponent, float, scope, name, type, action} = item
-          return <TableCell key={name} component={typeComponent} align={float} scope={scope}>
+    const classes = useStyles()
+    const {id, company, email, tel} = row
+    return (
+        <TableRow>
             {
-              row.isDisabled
-                ? <input
-                  type={type}
-                  name={name}
-                  placeholder={name === 'email' ? email : name === 'carrier' ? company : tel}
-                  onChange={action}
-                />
-                :
-                <span>{name === 'email' ? email : name === 'carrier' ? company : tel}</span>
+                tableCells.map((item) => {
+                    const {typeComponent, float, scope, name, type, action} = item
+                    return <TableCell key={name} component={typeComponent} align={float} scope={scope}>
+                        {
+                            row.isDisabled
+                                ? <input
+                                    type={type}
+                                    name={name}
+                                    placeholder={name === 'email' ? email : name === 'carrier' ? company : tel}
+                                    onChange={action}
+                                />
+                                :
+                                <span>{name === 'email' ? email : name === 'carrier' ? company : tel}</span>
+                        }
+                    </TableCell>
+                })
             }
-          </TableCell>
-        })
-      }
-      <TableCell align='right'>
-        {row.isDisabled
-          ? <Fab
-            color='primary'
-            aria-label='add'
-            className={classes.add_btn}
-            onClick={handleNewCarrier(id)}
-          >
-            <AddIcon/>
-          </Fab>
-          : <Fab
-            aria-label='edit'
-            className={classes.add_btn}
-            onClick={handleEdit(id)}
-          >
-            <EditIcon/>
-          </Fab>
-        }
-        <Fab
-          id={row.id}
-          onClick={removeItem(id)}
-          aria-label='delete'
-          className={classes.fab}
-          color='secondary'
-        >
-          <DeleteIcon/>
-        </Fab>
-      </TableCell>
-    </TableRow>
-  )
+            <TableCell align="right">
+                {row.isDisabled
+                    ? <Fab
+                        color="primary"
+                        aria-label="add"
+                        className={classes.add_btn}
+                        onClick={handleNewCarrier(id)}
+                    >
+                        <AddIcon/>
+                    </Fab>
+                    : <Fab
+                        aria-label="edit"
+                        className={classes.add_btn}
+                        onClick={handleEdit(id)}
+                    >
+                        <EditIcon/>
+                    </Fab>
+                }
+                <Fab
+                    id={row.id}
+                    onClick={removeItem(id)}
+                    aria-label="delete"
+                    className={classes.fab}
+                    color="secondary"
+                >
+                    <DeleteIcon/>
+                </Fab>
+            </TableCell>
+        </TableRow>
+    )
 }
 
 export default TableRowsComponent
